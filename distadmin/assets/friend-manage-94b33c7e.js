6b6ce4b4ed549f81e3203dd5d5d7fdd454b77170
yqj <yqj741452@outlook.com>
2023-07-02 22:29:19 +0800

    '最终代码'

--- distadmin/assets/friend-manage-94b33c7e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,a as n,i as o}from"./index-24cafc78.js";const t={},c={class:"friend"},a=o("h2",null,"friend",-1),r=[a];function _(d,i){return s(),n("div",c,r)}const l=e(t,[["render",_]]);export{l as default};
